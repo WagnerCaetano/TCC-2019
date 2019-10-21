2eaccf1a68be0380b4773d166e9bb1a20624cdc4
WagnerCaetano <40742874+WagnerCaetano@users.noreply.github.com>
2019-10-21 15:40:45 -0300

    ISlideeAndroid

---
+++ Aplicação/ISlideeAndroid/AndroidSlidee/app/src/main/java/com/example/androidslidee/Conectar.java
@@ -0,0 +1,64 @@
+package com.example.androidslidee;
+
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.List;
+
+public class Conectar extends AppCompatActivity {
+    WifiP2pManager manager;
+    WifiP2pManager.Channel channel;
+    WiFiDirectBroadcastReceiver receiver;
+
+    IntentFilter intentFilter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conectar);
+        manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        channel = manager.initialize(this, getMainLooper(), null);
+        receiver = new WiFiDirectBroadcastReceiver(manager, channel, this);
+
+        intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        manager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                System.out.println("Busca de peers com sucesso");
+            }
+
+            @Override
+            public void onFailure(int reasonCode) {
+                System.out.println("Busca de peers falhou");
+            }
+        });
+
+        try {
+            List listaPares = receiver.getPeers();
+            listaPares.add(listaPares);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(receiver, intentFilter);
+    }
+    /* unregister the broadcast receiver */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
+    }
+}
+
